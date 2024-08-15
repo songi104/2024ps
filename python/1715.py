03017f9ad4528f3bf3f2dcbb73e53648dfe55017
songi <10thddl04@gmail.com>
2024-08-15 21:37:54 +0900

    0815 heapq

--- python/1715.py
+++ python/1715.py
@@ -12,33 +12,53 @@ N^2-> 1e10이니까 100초인데
 
 """
 
-from collections import deque
+# from collections import deque
+# import sys
+# sys.stdin = open('input.txt', 'r')
+# input = sys.stdin.readline
+# N = int(input())
+# lst = []
+# for _ in range(N):
+#     lst.append(int(input()))
+
+# lst.(reverse=True)
+# # print(lst)
+# result = 0
+# temp = 0
+# a = lst.pop()
+# b = lst.pop()
+# temp = a+b
+# result += temp
+# target = []
+# while len(lst) > 1:
+#     while len(target) == 3:
+#         target.append(lst.pop())
+#     target.sort()
+#     # print(target)
+#     a, b, temp = target
+#     result += (a+b)
+
+# result += temp
+# if lst:
+#     result += lst[0]
+# print(result)
+
+
+from heapq import heapify, heappop, heappush
 import sys
 sys.stdin = open('input.txt', 'r')
 input = sys.stdin.readline
 N = int(input())
-lst = []
+heap = []
+# 뭐가 더 빠를까?
 for _ in range(N):
-    lst.append(int(input()))
-
-lst.sort(reverse=True)
-# print(lst)
-result = 0
-temp = 0
-a = lst.pop()
-b = lst.pop()
-temp = a+b
-result += temp
-target = []
-while len(lst) > 1:
-    while len(target) == 3:
-        target.append(lst.pop())
-    target.sort()
-    # print(target)
-    a, b, temp = target
-    result += (a+b)
-
-result += temp
-if lst:
-    result += lst[0]
-print(result)
+    heap.append(int(input()))
+heapify(heap)
+# print(heap)
+res = 0
+while len(heap) > 1:
+    a = heappop(heap)
+    b = heappop(heap)
+    res += a+b
+    heappush(heap, a+b)
+print(res)
