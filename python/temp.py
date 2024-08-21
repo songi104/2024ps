22acac86b4839d171bfbabcdf50e9d7da5d6c797
songi <10thddl04@gmail.com>
2024-08-21 14:02:48 +0900

    폴더이동

--- python/temp.py
+++ python/temp.py
@@ -2,9 +2,5 @@ import sys
 
 input = sys.stdin.readline
 
-A = int(input())
-B = int(input())
-C = int(input())
-
-print(A+B-C)
-print(int(str(A)+str(B))-C)
+words = list(input().split())
+print(len(words))
