3956e8635000c0cff8f0539e17f5de2414756b3f
songi <10thddl04@gmail.com>
2024-08-29 18:08:21 +0900

    0829

--- 백준py/temp.py
+++ 백준py/temp.py
@@ -1,22 +1,4 @@
 import sys
 
 input = sys.stdin.readline
-N = int(input())
-d = {}
-s = set()
-for _ in range(N):
-    num = int(input())
-    if num in s:
-        if num in d.keys():
-            d[num] += 1
-        else:
-            d[num] = 2
-    else:
-        s.add(num)
-
-for n in s:
-    if n in d.keys():
-        for _ in range(d[n]):
-            print(n)
-    else:
-        print(n)
+a = int(input())
