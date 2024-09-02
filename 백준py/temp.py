b76bae012c5f7514dce725587a06934e417cf09e
songi <10thddl04@gmail.com>
2024-09-02 23:48:42 +0900

    0902

--- 백준py/temp.py
+++ 백준py/temp.py
@@ -1,4 +1,19 @@
 import sys
 
+sys.stdin = open('input.txt', 'r')
 input = sys.stdin.readline
-a = int(input())
+T = int(input())
+for _ in range(T):
+    string = input().rstrip()
+    flag = False
+    res = 0
+    chunk = 0
+    for s in string:
+        if s == 'O':
+            chunk += 1
+            res += chunk
+            flag = True
+        else:  # X일 때
+            flag = False
+            chunk = 0
+    print(res)
