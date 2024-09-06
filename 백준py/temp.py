85b2774bd1bdcfb037b683eb93f6d60fbb7299a1
songi <10thddl04@gmail.com>
2024-09-06 20:35:07 +0900

    0906

--- 백준py/temp.py
+++ 백준py/temp.py
@@ -2,18 +2,14 @@ import sys
 
 sys.stdin = open('input.txt', 'r')
 input = sys.stdin.readline
-T = int(input())
-for _ in range(T):
-    string = input().rstrip()
-    flag = False
-    res = 0
-    chunk = 0
-    for s in string:
-        if s == 'O':
-            chunk += 1
-            res += chunk
-            flag = True
-        else:  # X일 때
-            flag = False
-            chunk = 0
-    print(res)
+
+lst = list(map(int, input().split()))
+ascending = sorted(lst)
+des = sorted(lst, reverse=True)
+
+if lst == ascending:
+    print('ascending')
+elif lst == des:
+    print('descending')
+else:
+    print('mixed')
