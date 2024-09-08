4c89ebadae5dbe36945f4b9c38de688649584558
songi <10thddl04@gmail.com>
2024-09-08 23:23:20 +0900

    0908

--- 백준py/temp.py
+++ 백준py/temp.py
@@ -1,19 +1,7 @@
 import sys
 
-sys.stdin = open('input.txt', 'r')
-input = sys.stdin.readline
 
-T = int(input())
-
-for _ in range(T):
-    H, W, N = map(int, input().split())
-    floor = N % H
-    house = N // H + 1
-    if floor == 0:
-        floor = H
-        house -= 1  # 이 경우에는 호수 계산에서 1을 빼야 함
-    if house < 10:
-        house = '0' + str(house)
-    else:
-        house = str(house)
-    print(str(floor) + house)
+lst = set()
+for _ in range(10):
+    lst.add(int(input()) % 42)
+print(len(lst))
