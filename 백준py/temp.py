eaae20464dedd7a706ec2271e33828ed355cf30d
songi <10thddl04@gmail.com>
2024-10-02 20:22:53 +0900

    1002

--- 백준py/temp.py
+++ 백준py/temp.py
@@ -1,7 +1,54 @@
+"""
+1. 아이디어
+다익스트라
 
-from string import ascii_lowercase
-lst = [1, 2, 3, 4]
-lst += [6]
-print(lst)
+2. 자료구조
+edges : int[][]
+dist: int[]
 
-hello
+
+3. 시간복잡도
+ElogV: 3e5lg(2e4)
+
+"""
+
+import heapq
+import sys
+
+input = sys.stdin.readline
+V, E = map(int, input().split())
+
+K = int(input())
+edges = [[] for _ in range(V+1)]
+# print(edges)
+for _ in range(E):
+    u, v, w = map(int, input().split())
+    edges[u].append((w, v))
+
+# print(edges)
+
+# 다익스트라
+hq = [(0, K)]
+INF = sys.maxsize
+dist = [INF]*(V+1)
+dist[K] = 0
+
+while hq:
+    ew, eu = heapq.heappop(hq)
+    if ew != dist[eu]:
+        continue
+
+    for nw, nv in edges[eu]:
+        if nw+ew < dist[nv]:
+            dist[nv] = nw+ew
+            heapq.heappush(hq, (ew+nw, nv))
+
+# print(dist)
+
+
+# dist 하나씩 출력
+for i in range(1, V+1):
+    if dist[i] == INF:
+        print("INF")
+    else:
+        print(dist[i])
