20048d0b85250132f2efa02c671eec8eca02fe40
songi <10thddl04@gmail.com>
2024-11-03 01:18:51 +0900

    cpp

--- cpp/7562.cpp
+++ cpp/7562.cpp
@@ -1,12 +1,64 @@
 #include <iostream>
+#include <queue>
+#define Y first
+#define X second
 
 using namespace std;
-int a, b, c;
-using ll = long long;
+
+int dy[8] = {-2, -2, 2, 2, -1, 1, -1, 1};
+int dx[8] = {1, -1, 1, -1, 2, 2, -2, -2};
+int graph[301][301];
+int t, l;
+
+int bfs(int a, int b, int c, int d) {
+    // 8가지 방향에 대해서
+    // 범위 체크, 방문했는지 체크, 그 후 update
+    queue<pair<int, int>> Q;
+    Q.push({a,b});
+    graph[a][b] = 0;
+
+    while (!Q.empty()) {
+        auto cur = Q.front(); Q.pop();
+        int cy = cur.Y;
+        int cx = cur.X;
+        if (cy == c && cx == d)
+            return graph[cy][cx];
+
+        for (int i =0; i<8; i++) {
+            int ny = cy + dy[i];
+            int nx = cx + dx[i];
+
+            if (ny < 0 || ny >= l || nx < 0 || nx >= l) continue;
+            if (graph[ny][nx] != -1) continue;
+            graph[ny][nx] = graph[cy][cx] + 1;
+            Q.push({ny, nx});
+
+            
+        }
+    }
+    return -1;
+}
+
 
 int main() {
-    cin >> a>> b>> c;
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
 
+    int a, b, c, d;
+    cin >> t;
+
+    while (t--) {
+        cin >> l;
+        cin >> a >> b; // 출발지
+        cin >> c >> d; // 목적지
+
+        for (int i=0 ; i<=l; i++ ) {
+            fill(graph[i], graph[i] +l, -1);
+        }
+
+        cout << bfs(a,b,c,d) << '\n';
+    }
 
 
     return 0;
---
+++ 소프티어/6248.cpp
@@ -0,0 +1,22 @@
+// 소프티어 6248번 출퇴근길
+
+#include <iostream>
+
+using namespace std;
+
+int n, m, s, t;
+int graph[100001][100001];
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    cin >> n >> m;
+    for (int i =0; i<m; i++) {
+        cin
+    }
+
+
+    return 0;
+}
